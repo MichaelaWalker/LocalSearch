5077d29243ece887c65111bc0bc19f2242a8c13d
hblanton57 <h.blanton57@gmail.com>
2024-02-15 14:45:55 -0500

    Add GeneticAlgorithm

---
+++ src/core_algorithms/GeneticAlgorithm.java
@@ -0,0 +1,50 @@
+package core_algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * implement elements that are independent of any specific problem
+ * */
+public class GeneticAlgorithm<G> {
+    private final int MAX_GEN;
+    private final double MUTATION_RATE;
+    private final double ELITISM;
+
+    public GeneticAlgorithm(int maxGen, double mRate, double elitism){
+        this.MAX_GEN = maxGen;
+        this.MUTATION_RATE = mRate;
+        this.ELITISM = elitism;
+    }
+
+    public Individual<G> evolve (List<Individual<G>> initPopulation){
+        List<Individual<G>> population = initPopulation;
+        for(int generation = 1; generation <= MAX_GEN; generation++){
+            List<Individual<G>> offspring = new ArrayList<>();
+            for(int i = 0; i < population.size(); i++){
+                Individual<G> p1 = selectAParent(population);
+                Individual<G> p2 = selectAParent(population, p1);
+                Individual<G> child = reproduce(p1, p2);
+                if (new Random().nextDouble() < MUTATION_RATE){ //rate will be between 0 and 1 (0% - 100%): value converted to probability
+                    child = mutate(child);
+                }
+                offspring.add(child);
+            }
+            Collections.sort(population);
+            Collections.sort(offspring);
+            List<Individual<G>> newPopulation = new ArrayList<>();
+            int e = (int)(ELITISM * population.size());
+            for(int i = 0; i < e; i++){
+                newPopulation.add(population.get(i));
+            }
+            for(int i = 0; i < population.size() - e; i++){
+                newPopulation.add(offspring.get(i));
+            }
+            population = newPopulation;
+        }//end of outer for loop
+
+        Collections.sort(population);
+    }
+}
